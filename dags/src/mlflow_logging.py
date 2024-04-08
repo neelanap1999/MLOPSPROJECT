d92a47a417069c0afae7f84fd30e42cd063c4cf2
Neel Shirish Anap <121322544+neelanap1999@users.noreply.github.com>
2024-04-08 16:02:47 -0400

    Update mlflow_logging.py

--- dags/src/mlflow_logging.py
+++ dags/src/mlflow_logging.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import os
@@ -18,9 +19,6 @@ from mlflow.models import infer_signature
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
-
-
-
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
@@ -39,30 +37,43 @@ if __name__ == "__main__":
     X_test=pd.read_parquet(XTEST_PATH)
     y_test=pd.read_parquet(YTEST_PATH)
     # The predicted column is "quality" which is a scalar from [3, 9]
+    n_estimators = [25, 50, 100, 150]
+    max_features = ['sqrt', 'log2', None]
+    max_depth = [3, 6, 9]
+    max_leaf_nodes = [3, 6, 9]
+    for a in n_estimators:
+        for b in max_features:
+            for c in max_depth:
+                for d in max_leaf_nodes:
 
-    with mlflow.start_run(run_name='RandomForestClassifier'):
-        lr = RandomForestClassifier(random_state=42)
-        lr.fit(X_train, y_train)
-        predicted_qualities = lr.predict(X_test)
-        print(f"  Accuracy: {accuracy_score(y_test,predicted_qualities)}")
+                    with mlflow.start_run(run_name='RandomForestClassifier'):
+                        lr = RandomForestClassifier(n_estimators=a, max_features=b, max_depth=c, max_leaf_nodes=d, random_state=42)
+                        lr.fit(X_train, y_train)
+                        y_pred = lr.predict(X_test)
+                        print(f"  Accuracy: {accuracy_score(y_test,y_pred)}")
+                        print(f"  Roc_auc_score: {roc_auc_score(y_test,y_pred)}")
 
+                        mlflow.log_param('n_estimators',a)
+                        mlflow.log_param('max_features',b)
+                        mlflow.log_param('max_depth',c)
+                        mlflow.log_param('max_leaf_nodes',d)
 
-        mlflow.log_metric("accuracy", accuracy_score(y_test,predicted_qualities))
-   
+                        mlflow.log_metric("accuracy", accuracy_score(y_test,y_pred))
+                        mlflow.log_metric('roc_auc_score',roc_auc_score(y_test,y_pred))
 
-        predictions = lr.predict(X_train)
-        signature = infer_signature(X_train, predictions)
+                        predictions = lr.predict(X_train)
+                        signature = infer_signature(X_train, predictions)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="RandomForesrClassifier", signature=signature
-            )
-        else:
-            mlflow.sklearn.log_model(lr, "model", signature=signature)
+                        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+                        
+                        # Model registry does not work with file store
+                        if tracking_url_type_store != "file":
+                            # Register the model
+                            # There are other ways to use the Model Registry, which depends on the use case,
+                            # please refer to the doc for more information:
+                            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                            mlflow.sklearn.log_model(
+                                lr, "model", registered_model_name="RandomForesrClassifier", signature=signature
+                            )
+                        else:
+                            mlflow.sklearn.log_model(lr, "model", signature=signature)
